c00b255097735d3eaa43a6200c6b15ebad3f5f89
harrisfauntleroy <harrisfauntleroy@gmail.com>
2020-11-22 12:37:03 +1000

    testing cloud build

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Main } from './components/Main';
 const App = () => {
 
   return (
-      <div className="container">
+    <div className="container">
+      <h1>test</h1>
         <Main></Main>
       </div>
   );
